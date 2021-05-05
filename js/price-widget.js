592671199bdfc0fc0747c55c25d79c4acd76ab62
Unagi <62870460+UnagiAdmin@users.noreply.github.com>
2021-05-05 16:59:17 +0800

    Update price-widget.js

--- js/price-widget.js
+++ js/price-widget.js
@@ -46,8 +46,8 @@ const init = async () => {
 
         // burnedSupplyEl.innerHTML = formatAmount(dead);
         marketCapEl.innerHTML = "approx. $" + formatAmount(Math.round(shortCirc * (shortPrice - 0)));
-        priceFor1ss = priceFor1BNB.slice(0, 15);
-        priceSpot1El.innerHTML = "approx. $" + priceFor1ss.toFixed(9);
+        priceFor1ss = priceFor1BNB.slice(0, 11);
+        priceSpot1El.innerHTML = "approx. $" + priceFor1ss;
 
     setTimeout(() => { init(); }, 5000);
 }
